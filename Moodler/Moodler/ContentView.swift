5d0a0ca00e250c8e929efa6a1e311a8f76d84cf0
chloedottruong <chloe.truong@student.uts.edu.au>
2025-10-15 23:06:20 +1100

    merging into main

---
+++ Moodler/Moodler/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Moodler
+//
+//  Created by Chloe on 15/10/2025.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+        
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
---
+++ Moodler/MoodlerTests/MoodlerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodlerTests.swift
+//  MoodlerTests
+//
+//  Created by Chloe on 15/10/2025.
+//
+
+import Testing
+@testable import Moodler
+
+struct MoodlerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
